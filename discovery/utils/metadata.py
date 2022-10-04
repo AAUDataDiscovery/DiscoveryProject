2a39c6fcda5db9072cf6f36a5b3cde523c0bd2dc
Balazs Mark Agardi <balazs@agardi.dk>
2022-10-04 15:22:47 +0200

    clean metadata DTO

--- discovery/utils/metadata.py
+++ discovery/utils/metadata.py
@@ -1,14 +1,20 @@
 """
 Data storage in memory
 """
+from typing import Optional
+
 import pandas
 
 
 class Metadata:
     filepath: str
-    dataframe: pandas.DataFrame
+    mean: Optional[int, float]
+    min: any
+    max: any
 
-
-    def __init__(self, filepath, dataframe):
+    def __init__(self, filepath: str, mean: Optional[int, float], min_val, max_val):
         self.filepath = filepath
-        self.dataframe = dataframe
+        self.mean = mean
+        self.min = min_val
+        self.max = max_val
+
