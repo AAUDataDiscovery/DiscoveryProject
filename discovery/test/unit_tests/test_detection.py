7c09ec6c54be23747dd04c2b6d14a79a2638ca3a
MII <mnel.jnr@gmail.com>
2022-10-10 13:11:31 +0200

    Switched to temp fs to prevent tests from being relative

--- discovery/test/unit_tests/test_detection.py
+++ discovery/test/unit_tests/test_detection.py
@@ -1,17 +1,18 @@
 """
 Ensure that the discovery system can detect and load test data
 """
+import os
+
 from _pytest.fixtures import fixture
 from discovery import Discovery
 from utils.datagen import FakeDataGen
 
-
-@fixture
-def mock_filesystem():
+@fixture(scope="session")
+def mock_data_folder(tmp_path_factory):
     generate_data = FakeDataGen()
-    new_files = generate_data.build_df_to_file(10000, path='test/mock_filesystem/simple_test',
-                                               continuous_data=10, categoric_data=10, file_spread=5)
-    return new_files
+    temp_path = tmp_path_factory.mktemp("mock_fs")
+    generate_data.build_df_to_file(10000, path=temp_path, continuous_data=10, categoric_data=10, file_spread=5)
+    return temp_path
 
 
 @fixture
@@ -19,6 +20,6 @@ def discovery_instance():
     return Discovery({})
 
 
-def test_simple_detection(discovery_instance, mock_filesystem):
-    discovery_instance.add_files("test/mock_filesystem")
-    assert all([x in list(discovery_instance.get_loaded_files()) for x in mock_filesystem])
+def test_simple_detection(discovery_instance, mock_data_folder):
+    discovery_instance.add_files(str(mock_data_folder))
+    assert all(x in list(discovery_instance.get_loaded_files()) for x in os.listdir(mock_data_folder))
