55dc87042838590018d59eb505994725e7a35c4d
MII <mnel.jnr@gmail.com>
2022-10-10 11:47:52 +0200

    Moved test structure

--- discovery/test/unit_tests/test_detection.py
+++ discovery/test/unit_tests/test_detection.py
@@ -2,14 +2,14 @@
 Ensure that the discovery system can detect and load test data
 """
 from _pytest.fixtures import fixture
-from test.datagen import FakeDataGen
 from discovery import Discovery
+from utils.datagen import FakeDataGen
 
 
 @fixture
 def mock_filesystem():
     generate_data = FakeDataGen()
-    new_files = generate_data.build_df_to_file(10000, path='mock_filesystem/simple_test',
+    new_files = generate_data.build_df_to_file(10000, path='test/mock_filesystem/simple_test',
                                                continuous_data=10, categoric_data=10, file_spread=5)
     return new_files
 
@@ -20,5 +20,5 @@ def discovery_instance():
 
 
 def test_simple_detection(discovery_instance, mock_filesystem):
-    discovery_instance.add_files("mock_filesystem")
+    discovery_instance.add_files("test/mock_filesystem")
     assert all([x in list(discovery_instance.get_loaded_files()) for x in mock_filesystem])
