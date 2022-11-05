3863b67ac7abd69607fa81fb9d50a6feb910b7a1
Alex Bursuc <bursuc.alex99@gmail.com>
2022-11-05 14:59:51 +0100

    export csv class & remove empty rows function

--- discovery/utils/split_data.py
+++ discovery/utils/split_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import dask.dataframe as dd
 import time
 
+
 class SplitData:
      def  read_csv(self, file_path, npartitions):
           
@@ -10,41 +11,44 @@ class SplitData:
                sep=',')
           
           shuffle=ddf.sample(frac=1)
-          self.partition = shuffle.repartition(npartitions)
+          
+          partition = shuffle.repartition(npartitions)
+          
+          object_data = partition.select_dtypes(include=['object'])
+
+          self.object = object_data.dropna(thresh=1)
+          
+          float_data = partition.select_dtypes(include=['float64'])
+
+          self.float = float_data.dropna(thresh=1)
 
           """
           Read, shuffle and partition the CSV/multiple CSVs
           """
      
-     def split_object_data(self, file_export):
-          object = self.partition.select_dtypes(include=['object'])
-          object.to_csv(
+
+     def export_csv(self, file_export):
+          self.object.to_csv(
                file_export +"data_object_*.csv", 
                index=False, 
                mode='w', 
                header = True)
 
-          """
-          Exports only the string data type 
-          """
-     
-     def split_float_data(self, file_export):
-          float = self.partition.select_dtypes(include=['float64'])
-          float.to_csv(
+          self.float.to_csv(
                file_export + "data_float_*.csv", 
                index=False, 
                mode='w', 
                header = True)
 
           """
-          Exports only the float data type 
+          Exports only the object & float data in separate CSVs
           """
+
 
 if __name__ == "__main__":
     splitdatagen = SplitData()
     start_time = time.time()
     splitdatagen.read_csv(file_path="mock_filesystem/new_test_*.csv", npartitions=5)
-    splitdatagen.split_object_data(file_export="mock_filesystem/")
-    splitdatagen.split_float_data(file_export="mock_filesystem/")
+    splitdatagen.export_csv(file_export="mock_filesystem/")
 
     print(f"Completed data generation in {time.time() - start_time} seconds")
